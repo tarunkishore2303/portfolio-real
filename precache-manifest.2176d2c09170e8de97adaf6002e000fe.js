4c9bac229810e0f8613ab9012bc84c3e54c10f5a
Tarun Kishore <tarunksu30@gmail.com>
2022-02-07 09:18:01 +0530

    Updates

---
+++ precache-manifest.2176d2c09170e8de97adaf6002e000fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6140f1052f10075f002910aa8b6a73d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "faaab065cec333a968c2",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "089d487baed7c13ecb69",
+    "url": "./static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "faaab065cec333a968c2",
+    "url": "./static/js/2.9bc8f475.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.9bc8f475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "089d487baed7c13ecb69",
+    "url": "./static/js/main.6eb3f27a.chunk.js"
+  },
+  {
+    "revision": "36035a646046245b73df",
+    "url": "./static/js/runtime-main.5b3e0ee1.js"
+  }
+]);
